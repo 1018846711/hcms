c53c3b83fee51dfd1e09799a3e5e2a3cd6f3ac52
mujianhua <1018846711@qq.com>
2018-07-30 10:26:57 +0800

    test

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,8 +6,13 @@ use think\Controller;
 
 class Index extends Controller {
     public function index() {
-        echo request()->ip(1);
+        echo request()->ip(0);
         echo "</br>";
         echo phpinfo();
+    }
+
+    public function test() {
+
+
     }
 }
